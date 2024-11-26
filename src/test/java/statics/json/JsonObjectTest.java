ae151b49149a34e1d1363932619289153df2add1
Adrian <adrian.akipi@web.de>
2024-11-26 01:17:39 +0100

    test enhancement

--- src/test/java/statics/json/JsonObjectTest.java
+++ src/test/java/statics/json/JsonObjectTest.java
@@ -11,6 +11,11 @@ class JsonObjectTest {
         JsonObject ob1 = new JsonObject();
 
         ob1.setField("string",new JsonObject.JsonValue<String>("eier lecken"));
+
+        JsonObject.JsonValueList jvl2 = new JsonObject.JsonValueList();
+        jvl2.add(new JsonObject.JsonValue<String>("str"));
+        ob1.setField("stringlist", new JsonObject.JsonValue<JsonObject.JsonValueList>(jvl2));
+
         ob1.setField("double",new JsonObject.JsonValue<Double>(1_3.3_4d));
         ob1.setField("boolean",new JsonObject.JsonValue<Boolean>(true));
 
@@ -18,13 +23,16 @@ class JsonObjectTest {
         ob2.setField("s", new JsonObject.JsonValue<String>("EEEE"));
         ob1.setField("object",new JsonObject.JsonValue<JsonObject>(ob2));
 
-        JsonObject.JsonValueList jvl = new JsonObject.JsonValueList();
-        jvl.add(new JsonObject.JsonValue<Double>(3d));
-        jvl.add(new JsonObject.JsonValue<Double>(30d));
-        jvl.add(new JsonObject.JsonValue<Double>(33d));
-        jvl.add(new JsonObject.JsonValue<Double>(32d));
-        jvl.add(new JsonObject.JsonValue<Double>(12d));
-        ob2.setField("numlist", new JsonObject.JsonValue<JsonObject.JsonValueList>(jvl));
+
+
+        JsonObject.JsonValueList jvl1 = new JsonObject.JsonValueList();
+        jvl1.add(new JsonObject.JsonValue<Double>(3d));
+        jvl1.add(new JsonObject.JsonValue<Double>(30d));
+        jvl1.add(new JsonObject.JsonValue<Double>(33d));
+        jvl1.add(new JsonObject.JsonValue<Double>(32d));
+        jvl1.add(new JsonObject.JsonValue<Double>(12d));
+        ob2.setField("numlist", new JsonObject.JsonValue<JsonObject.JsonValueList>(jvl1));
+
         System.out.println(ob1.toString());
     }
 }
