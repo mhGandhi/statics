efdf37e4487146b9aa47581a83060481770f9fe4
Adrian <adrian.akipi@web.de>
2024-11-28 23:21:01 +0100

    added constructor

--- src/main/java/statics/app/ActionHandler.java
+++ src/main/java/statics/app/ActionHandler.java
@@ -1,9 +1,18 @@
 package statics.app;
 
+import statics.app.view.ViewState;
+
 /**
  * handles all actions in the View
  *
  * @author Adrian Akipi
  *///todo comment
 public class ActionHandler implements IActionHandler{
+    private App app;
+    private ViewState viewState;
+
+    public ActionHandler(App pApp, ViewState pViewState) {
+        this.app = pApp;
+        this.viewState = pViewState;
+    }
 }
