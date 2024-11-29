46869d60c3cc1c65cfaa512e827635b36abbede6
Adrian <adrian.akipi@web.de>
2024-11-29 01:02:29 +0100

    added painting logic

--- src/main/java/statics/app/StaticsWindow.java
+++ src/main/java/statics/app/StaticsWindow.java
@@ -9,15 +9,22 @@ import java.awt.*;
 
 //todo comment
 public class StaticsWindow extends JFrame implements IView {
-    StaticsPanel panel;
+    private ViewState vs;
+    private StaticsPanel panel;
 
     public StaticsWindow(ViewState pViewState, IActionHandler pActionHandler) {
-        this.setSize(500,500);//todo settings
-        this.setLayout(new BorderLayout());
+        this.vs = pViewState;
+        {
+            this.setSize(500,500);//todo settings
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setLayout(new BorderLayout());
+        }
 
         this.panel = new StaticsPanel(pViewState, pActionHandler);
 
         this.add(this.panel,BorderLayout.CENTER);
+
+
         this.setVisible(true);
     }
 }
--- src/main/java/statics/app/view/StaticsPanel.java
+++ src/main/java/statics/app/view/StaticsPanel.java
@@ -1,14 +1,81 @@
 package statics.app.view;
 
 import statics.app.IActionHandler;
+import statics.app.model.SystemPos;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 //todo comment
 public class StaticsPanel extends JPanel {
+    private final ViewState vs;
 
     public StaticsPanel(ViewState pViewState, IActionHandler pActionHandler) {
-        this.setBackground(Color.GREEN);
+        this.vs = pViewState;
+        this.setFocusable(true);
+
+        this.addMouseMotionListener((MouseMotionListener) pActionHandler);
+        this.addMouseListener((MouseListener) pActionHandler);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        //g2d.addRenderingHints(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+
+        {//draw background
+            {//bg color
+                GradientPaint bgPaint = (GradientPaint) vs.getColorScheme().background;
+                GradientPaint bgPaintWithCoordinates = new GradientPaint(
+                        -getWidth(),-getHeight(),bgPaint.getColor1(),
+                        getWidth()*2,getHeight()*2,bgPaint.getColor2()
+                );
+                g2d.setPaint(bgPaintWithCoordinates);
+
+                g2d.fillRect(0,0,getWidth(),getHeight());
+            }
+            {//bg grid
+                g2d.setPaint(vs.getColorScheme().drawBg2);
+                int gridSize = 1;
+
+
+                {//vertical gridlines
+                    for(
+                            int i = -vs.getOffX();
+                            i <= getWidth();
+                            i += vs.toScreenPos(new SystemPos(10,0)).getX()*gridSize
+                    ){
+                        g2d.drawLine(i,0,i,getHeight());
+                    }
+                }
+                /*
+                for (int i = (int)Math.round((-viewState.getOffsetX())%(2*(viewState.getZoom()*1))); i < getBounds().getWidth(); i+=2*(int)Math.round(viewState.getZoom()*1)) {
+                    g2d.drawLine(i, 0, i, (int)getBounds().getHeight());
+                    if(viewState.getZoom()>=20&&i>20){
+                        Font f = new Font(getViewState().getTheme().getFont(), Font.PLAIN, 20);
+                        g2d.setFont(f);
+                        g2d.drawString(""+(Math.round((viewState.getOffsetX()+i)/viewState.getZoom())), i+1, 20);
+                    }
+                }
+                */
+                {//horizontal gridlines
+
+                }
+                /*
+                for (int i = (int)Math.round((-viewState.getOffsetY())%(2*(viewState.getZoom()*1))); i < getBounds().getHeight(); i+=2*(int)Math.round(viewState.getZoom()*1)) {
+                    g2d.drawLine(0, i, (int)getBounds().getWidth(), i);
+                    if(viewState.getZoom()>=20&&i>20){
+                        g2d.drawString(""+(Math.round((viewState.getOffsetY()+i)/viewState.getZoom())), 0, i-1);
+                    }
+                }
+                */
+            }
+
+            ScreenPos tmp = vs.toScreenPos(new SystemPos(0,0));
+            g2d.fillOval(tmp.getX(),tmp.getY(),vs.getScale(),vs.getScale());
+        }
     }
 }
