218429afdce56b7cb961ce49c1ca07685ed1825d
Adrian <adrian.akipi@web.de>
2024-12-02 16:30:35 +0100

    idk

--- src/main/java/statics/app/view/components/IComponent.java
+++ src/main/java/statics/app/view/components/IComponent.java
@@ -5,10 +5,10 @@ import statics.app.view.RedrawModes;
 import java.awt.*;
 
 //todo
-public interface IComponent {
+public interface IComponent{
     Rectangle getBounds();
 
     void draw(Graphics2D g2d, RedrawModes redrawMode);
 
-    int getLayer();
+    ComponentLayers getLayer();
 }
