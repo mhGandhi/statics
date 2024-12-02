5870efe56cc3d92e698de188957f14fd0300de0e
Adrian <adrian.akipi@web.de>
2024-12-02 16:30:27 +0100

    extracted bg grid to seperate component

--- src/main/java/statics/app/view/StaticsPanel.java
+++ src/main/java/statics/app/view/StaticsPanel.java
@@ -2,6 +2,7 @@ package statics.app.view;
 
 import statics.app.IActionHandler;
 import statics.app.model.SystemPos;
+import statics.app.view.components.Grid;
 import statics.app.view.components.IComponent;
 
 import javax.swing.*;
@@ -26,6 +27,8 @@ public class StaticsPanel extends JPanel {
 
         this.addMouseMotionListener((MouseMotionListener) pActionHandler);
         this.addMouseListener((MouseListener) pActionHandler);
+
+        components.add(new Grid(vs));
     }
 
     @Override
@@ -34,56 +37,17 @@ public class StaticsPanel extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         //g2d.addRenderingHints(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 
+
         {//draw background
             {//bg color
                 GradientPaint bgPaint = (GradientPaint) vs.getColorScheme().background;
                 GradientPaint bgPaintWithCoordinates = new GradientPaint(
-                        -getWidth(),-getHeight(),bgPaint.getColor1(),
-                        getWidth()*2,getHeight()*2,bgPaint.getColor2()
+                        -getWidth(), -getHeight(), bgPaint.getColor1(),
+                        getWidth() * 2, getHeight() * 2, bgPaint.getColor2()
                 );
                 g2d.setPaint(bgPaintWithCoordinates);
 
-                g2d.fillRect(0,0,getWidth(),getHeight());
-            }
-            {//bg grid
-                g2d.setPaint(vs.getColorScheme().backgroundAnnotation);
-                double gridSize = 2.5;
-                {//vertical gridlines
-                    for(
-                            int i = -vs.getOffX()%(int)Math.round(gridSize* vs.getScale());
-                            i <= getWidth();
-                            i += (int)Math.round(gridSize*vs.getScale())
-                    ){
-                        g2d.drawLine(i,0,i,getHeight());
-                    }
-                }
-                /*//todo add some way to display coords
-                for (int i = (int)Math.round((-viewState.getOffsetX())%(2*(viewState.getZoom()*1))); i < getBounds().getWidth(); i+=2*(int)Math.round(viewState.getZoom()*1)) {
-                    g2d.drawLine(i, 0, i, (int)getBounds().getHeight());
-                    if(viewState.getZoom()>=20&&i>20){
-                        Font f = new Font(getViewState().getTheme().getFont(), Font.PLAIN, 20);
-                        g2d.setFont(f);
-                        g2d.drawString(""+(Math.round((viewState.getOffsetX()+i)/viewState.getZoom())), i+1, 20);
-                    }
-                }
-                */
-                {//horizontal gridlines
-                    for(
-                            int i = -vs.getOffY()%(int)Math.round(gridSize* vs.getScale());
-                            i <= getHeight();
-                            i += (int)Math.round(gridSize*vs.getScale())
-                    ){
-                        g2d.drawLine(0,i,getWidth(),i);
-                    }
-                }
-                /*//todo add some way to display coords
-                for (int i = (int)Math.round((-viewState.getOffsetY())%(2*(viewState.getZoom()*1))); i < getBounds().getHeight(); i+=2*(int)Math.round(viewState.getZoom()*1)) {
-                    g2d.drawLine(0, i, (int)getBounds().getWidth(), i);
-                    if(viewState.getZoom()>=20&&i>20){
-                        g2d.drawString(""+(Math.round((viewState.getOffsetY()+i)/viewState.getZoom())), 0, i-1);
-                    }
-                }
-                */
+                g2d.fillRect(0, 0, getWidth(), getHeight());
             }
 
             g2d.setColor(Color.RED);//todo remove
@@ -100,6 +64,8 @@ public class StaticsPanel extends JPanel {
                 }
             }
         }
+
+
     }
 
     public void repaint(RedrawModes pRedrawMode) {
