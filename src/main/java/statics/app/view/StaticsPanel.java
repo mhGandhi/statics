7df68c2c6a637706c16fa63ce0be7c99414711e9
Adrian <adrian.akipi@web.de>
2024-12-03 02:42:18 +0100

    antialiasing

--- src/main/java/statics/app/view/StaticsPanel.java
+++ src/main/java/statics/app/view/StaticsPanel.java
@@ -38,7 +38,7 @@ public class StaticsPanel extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        //g2d.addRenderingHints(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 
         {//draw background
